f6eff714f8ef964f2b31487a34378ec271b85467
Reid Vandewiele <reid@puppetlabs.com>
2014-11-21 08:56:08 -0800

    Generalize http method logic, use tempfile
    In preparation for performing HTTP HEAD requests, generalize the method
    containing the Net::HTTP logic. This commit moves the logic from the
    http_get() method to a new method, http(), which accepts an IO object to
    write the body to and returns the HTTP response object for any request
    made. It supports performing HEAD or other request types via the options
    parameter.
    
    While making these changes it was convenient to introduce a tempfile
    download pattern, wherein rather than saving directly to the target
    path, the provider will create a tempfile and mv it to the target path
    upon successful download. This pattern prevents incomplete files from
    being dropped into the target path.

--- lib/puppet/provider/remote_file/ruby.rb
+++ lib/puppet/provider/remote_file/ruby.rb
@@ -11,6 +11,11 @@ Puppet::Type.type(:remote_file).provide(:ruby, :parent => Puppet::Provider::Remo
 
   mk_resource_methods
 
+  REQUEST_TYPES = {
+    'get'  => Net::HTTP::Get,
+    'head' => Net::HTTP::Head,
+  }
+
   def create
     get @resource[:source]
     validate_checksum if checksum_specified?
@@ -34,37 +39,99 @@ Puppet::Type.type(:remote_file).provide(:ruby, :parent => Puppet::Provider::Remo
     ! specified_checksum.nil?
   end
 
-  def get(url, i=0)
+  # Determine and begin the appropriate method of getting the target file.
+  #
+  def get(url)
     p = URI.parse url
     case p.scheme
     when /https?/
-      http_get p, i
+      http_get p, @resource[:path]
     when "file"
       FileUtils.copy p.path, @resource[:path]
     end
   end
 
-  def http_get(p, i=0)
-    if i > 5
-      raise Puppet::Error.new "Redirected more than 5 times when trying to download #{@resource[:path]}, aborting."
+  # Perform an HTTP GET request, saving the body in the specified download
+  # path, and return the result.
+  #
+  def http_get(uri, download_path)
+
+    # We'll save to a tempfile in case something goes wrong in the download
+    # process. This avoids accidentally overwriting an old version, or
+    # leaving a partially downloaded file in place
+    begin
+      tempfile = Tempfile.new('remote_file')
+      response = http(uri, tempfile)
+
+      # If download was successful, copy the tempfile over to the resource path.
+      if response.kind_of?(Net::HTTPSuccess)
+        tempfile.flush
+        FileUtils.mv(tempfile.path, download_path)
+      end
+    ensure
+      tempfile.close
+      tempfile.unlink
+      response
     end
-    c = Net::HTTP.new(p.host, p.port)
-    c.use_ssl = p.scheme == "https" ? true : false
-    if c.use_ssl? and @resource[:verify_peer] == false
-      c.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  end
+
+  # Use Net::HTTP to perform a request against a webserver, following
+  # redirects, and return the final response. This method accepts a uri, an io
+  # object to which the response body will be saved in the event of an
+  # HTTPSuccess response from the webserver, and an optional hash of options.
+  # The method will follow HTTPRedirect codes up to 10 times, and will return
+  # the final HTTPResponse.
+  #
+  # @param uri [URI::HTTP] the uri to perform the request against
+  # @param io_ready_to_write [IO] an open file or other IO object ready to
+  #        write. The response body will be written to this object.
+  # @param options [Hash] a hash of options to adjust behavior
+  #
+  def http(uri, io_ready_to_write, options = {})
+    verb    = options[:http_method] || 'get'
+    limit   = options[:limit]       || 10
+
+    raise ArgumentError, 'HTTP redirect too deep' if limit == 0
+
+    # Create the Net::HTTP connection and  request objects
+    connection = Net::HTTP.new(uri.host, uri.port)
+    request    = REQUEST_TYPES[verb.to_s.downcase].new(uri.request_uri)
+
+    # Configure the Net::HTTP connection object
+    if uri.scheme == 'https'
+      connection.use_ssl = true
     end
-    c.request_get(p.request_uri) do |req|
-      case req.code
-      when /30[12]/
-        get req['location'], i+1
-      when "200"
-        File.open(@resource[:path], 'w') do |fh|
-          req.read_body { |buf| fh.write buf }
-          fh.flush
+
+    if connection.use_ssl? and @resource[:verify_peer] == false
+      connection.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    end
+
+    # Configure the Net::HTTPRequest object
+    if options[:headers]
+      options[:headers].each {|key,value| request[key] = value }
+    end
+
+    # Connect and perform the request
+    http_method = connection.method("request_#{verb.downcase}".to_sym)
+    recursive_response = nil
+    response = connection.start do |http|
+      http.request(request) do |resp|
+        # Determine and react to the request result
+        case resp
+        when Net::HTTPRedirection
+          next_opts = options.merge(:limit => limit - 1)
+          next_loc  = URI.parse(resp['location'])
+          recursive_response = http(next_loc, io_ready_to_write, next_opts)
+        when Net::HTTPSuccess
+          resp.read_body do |chunk|
+            io_ready_to_write.write(chunk)
+          end
+        else
+          raise Puppet::Error.new "Unexpected response code #{resp.code}: #{resp.read_body}"
         end
-      else
-        raise Puppet::Error.new "Unexpected response code #{req.code}: #{req.read_body}"
       end
     end
+
+    recursive_response || response
   end
 end
