634cd308879f4abfa63412e7d2148f04b671df77
Reid Vandewiele <reid@puppetlabs.com>
2018-04-25 10:12:45 -0700

    Convert module to PDK
    The PDK provides a standard framework for style, building and running
    tests, and maintaining metadata. While there are no tests currently
    written for this module, converting to the PDK lowers the barrier to
    entry for writing them should anyone want to do that in the future.
    
    As part of converting to the PDK many Ruby style edits were made in
    order to align with the conventional rubocop linting and style tests.
    
    Because Ruby 1.9.x conventions are used, the supported version of Puppet
    is changed from >=3.x to >=4.6.x. Older versions may work but it is
    difficult to find documentation for versions of Puppet older than 4.6.
    The oldest version of Ruby supported by Puppet 4.6 is 1.9.3.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,30 @@
-dir = File.expand_path(File.dirname(__FILE__))
-$LOAD_PATH.unshift File.join(dir, 'lib')
+require 'puppetlabs_spec_helper/module_spec_helper'
+require 'rspec-puppet-facts'
 
-require 'mocha'
-require 'puppet'
-require 'rspec'
-require 'spec/autorun'
-
-Spec::Runner.configure do |config|
-    config.mock_with :mocha
+begin
+  require 'spec_helper_local' if File.file?(File.join(File.dirname(__FILE__), 'spec_helper_local.rb'))
+rescue LoadError => loaderror
+  warn "Could not require spec_helper_local: #{loaderror.message}"
 end
 
-# We need this because the RAL uses 'should' as a method.  This
-# allows us the same behaviour but with a different method name.
-class Object
-    alias :must :should
+include RspecPuppetFacts
+
+default_facts = {
+  puppetversion: Puppet.version,
+  facterversion: Facter.version,
+}
+
+default_facts_path = File.expand_path(File.join(File.dirname(__FILE__), 'default_facts.yml'))
+default_module_facts_path = File.expand_path(File.join(File.dirname(__FILE__), 'default_module_facts.yml'))
+
+if File.exist?(default_facts_path) && File.readable?(default_facts_path)
+  default_facts.merge!(YAML.safe_load(File.read(default_facts_path)))
+end
+
+if File.exist?(default_module_facts_path) && File.readable?(default_module_facts_path)
+  default_facts.merge!(YAML.safe_load(File.read(default_module_facts_path)))
+end
+
+RSpec.configure do |c|
+  c.default_facts = default_facts
 end
